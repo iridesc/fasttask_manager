e3e460f3181762d40a439c44db8e9b461ec1517c
zhangziyuan <zhangziyuan@nsfocus.com>
2025-06-18 16:36:23 +0800

    优化测试

--- test.py
+++ test.py
@@ -5,17 +5,41 @@ if __name__ == "__main__":
     m = Manager(
         "127.0.0.1",
         task_name="get_hypotenuse",
-        port=80,
+        port=9001,
+        protocol="https",
         auth_user="john wick",
         auth_passwd="john_passwd",
     )
 
-    result = m.run(params)
-    print(f"{result=}")
+    r = m.run(params)
+    print(f"m.run {r=}")
+    print("---------------------")
 
     file_path = m.upload("README.md")
-    print(f"{file_path=}")
-    m.download(file_path, "./download_README.md")
+    print(f"m.upload {file_path=}")
+    print("---------------------")
 
-    result = m.create_and_wait_result(params)
-    print(result)
+    m.download(file_path, "./download_README.md")
+    print("m.download done")
+    print("---------------------")
+
+    r = m.create_and_wait_result(params)
+    print(f"m.create_and_wait_result {r=}")
+    print("---------------------")
+
+    r = m.create_task(params)
+    print(f"m.create_task {r=}")
+    print("---------------------")
+
+    result_id = r["id"]
+    r = m.check(result_id)
+    print(f"m.check {r=}")
+    print("---------------------")
+
+    r = m.revoke(result_id)
+    print(f"m.revoke {r=}")
+    r = m.check(result_id)
+    assert r["state"] == "REVOKED"
+    print("---------------------")
+
+    print("done")
