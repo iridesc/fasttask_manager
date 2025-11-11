8494992c79f211898779ef0b4305896702c8637a
zhangziyuan <zhangziyuan@nsfocus.com>
2025-11-11 16:28:35 +0800

    优化了同步测试代码

--- test.py
+++ test.py
@@ -1,45 +1,46 @@
+import os
 from fasttask_manager import Manager
 
 if __name__ == "__main__":
+    task_name = "get_hypotenuse"
     params = {"a": 4, "b": 3}
     m = Manager(
         "127.0.0.1",
-        task_name="get_hypotenuse",
         port=9001,
         protocol="https",
         auth_user="john wick",
         auth_passwd="john_passwd",
     )
 
-    r = m.run(params)
-    print(f"m.run {r=}")
+    r = m.run(task_name, params)
+    assert r["state"] == "SUCCESS" and r["result"]["hypotenuse"] == 5.0
     print("---------------------")
 
     file_path = m.upload("README.md")
-    print(f"m.upload {file_path=}")
+    assert file_path.endswith("README.md")
     print("---------------------")
-
+    if os.path.exists("./download_README.md"): 
+        os.remove("./download_README.md")
     m.download(file_path, "./download_README.md")
-    print("m.download done")
+    assert os.path.exists("./download_README.md")
     print("---------------------")
 
-    r = m.create_and_wait_result(params)
-    print(f"m.create_and_wait_result {r=}")
+    r = m.create_and_wait_result(task_name, params, check_gap=1, simple_error_log=False, log_prefix="test:")
+    assert r["hypotenuse"] == 5.0
     print("---------------------")
 
-    r = m.create_task(params)
-    print(f"m.create_task {r=}")
+    r = m.create_task(task_name, params)
+    assert r["state"] == "PENDING"
     print("---------------------")
 
     result_id = r["id"]
-    r = m.check(result_id)
-    print(f"m.check {r=}")
+    r = m.check(task_name, result_id)
+    assert r["state"] in ["STARTED", "PENDING"]  
     print("---------------------")
 
     r = m.revoke(result_id)
-    print(f"m.revoke {r=}")
-    r = m.check(result_id)
-    print(f"m.revoke check {r=}")
+    assert r["status"] == "SUCCESS"
+    r = m.check(task_name, result_id)
     assert r["state"] == "REVOKED"
     print("---------------------")
 
