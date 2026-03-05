1c167b36063a6095e2411ed51589a44dd9581753
zhangziyuan <zhangziyuan@nsfocus.com>
2026-03-05 18:06:05 +0800

    优化了异步manager, 增加了基于环境变量的 debug 模式配置， 在debug 下 会输出响应体

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fasttask_manager",
-    version="0.4.1",
+    version="0.5.0",
     author="Irid",
     author_email="irid.zzy@gmail.com",
     description="fasttask's manager ",
